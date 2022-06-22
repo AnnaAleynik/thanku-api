d0dd75d1f9e411aba69bbfa07750e06cd892f2a6
AnnaAleynik <anna.aleynik@flatstack.com>
2022-06-22 21:41:37 +0300

    fix

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -3,5 +3,5 @@ FactoryBot.define do
     name { "#{FFaker::Company.name}#{rand(1..100)}" }
     description { FFaker::Company.catch_phrase }
     bonus_amount { 500 }
-  end
+  end              
 end
